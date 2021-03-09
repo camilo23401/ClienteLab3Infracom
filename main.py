750910133d2108c24db342efaea81723cd6db5ee
unknown <lh.cabra@uniandes.edu.co>
2021-03-09 16:30:23 -0500

    pequeño avance en cliente, falta identificacion

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import socket
 import os
 import hashlib
+from datetime import datetime
 
 host = 'localhost'
 port = 8050
 path_archivos = "./ArchivosRecibidos"
-nombre_archivo =  "Cliente1-Prueba-5.txt"
-BLOCK_SIZE = 65536
+path_logs = "./Logs"
 
+BLOCK_SIZE = 65536
+conexiones=25
 
 obj = socket.socket()
 
 obj.connect((host, port))
-obj.listen(25)
+obj.listen(conexiones)
+
+archivolog = os.path.join(path_logs, datetime.now())
+file = open(archivolog, "w")
+
+#No se me ocurrió otra manera de identificar el cliente
+cliente=obj.__getattribute__('name')
+
 print("Conectado al servidor")
 
 
@@ -20,10 +29,15 @@ while True:
     mens = input("Mensaje de confirmacion. Listo para recibir un archivo")
     obj.send(mens.encode('ascii'))
     recibido = obj.recv(4096)
+#    log="nombre del archivo: "
+#    log=log+" Tamaño: "+os.stat(recibido).st_size
+#    log=log+"\n Cliente: "+cliente
+    nombre_archivo = cliente+"-Prueba"+conexiones
     archivoPorEscribir = os.path.join(path_archivos, nombre_archivo)
     file1 = open(archivoPorEscribir, "w")
     file1.write(recibido)
     file1.close()
+#    log=log+"\n Entrega exitosa: "
 
 
     file_hash = hashlib.sha256()
@@ -33,6 +47,8 @@ while True:
             file_hash.update(fb)
             fb = f.read(BLOCK_SIZE)
     resultadoHash = file_hash.hexdigest()
+#log=log+"\n Tiempo de transferencia: "
+#log=log+"\n Número de paquetes recibidos: "++" número de bytes recibidos: "
 obj.close()
 print("Conexión cerrada")
 
