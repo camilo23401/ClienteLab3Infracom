c62a3d30c6c7c42ec2786bee3d749058cca5272a
unknown <lh.cabra@uniandes.edu.co>
2021-03-10 15:54:08 -0500

    threads y avance en log

--- main.py
+++ main.py
@@ -2,58 +2,71 @@ import socket
 import os
 import hashlib
 from datetime import datetime
-
-host = 'localhost'
-port = 8050
-path_archivos = "./ArchivosRecibidos"
-path_logs = "./Logs"
-
-BLOCK_SIZE = 65536
-conexiones=25
-
-obj = socket.socket()
-
-obj.connect((host, port))
-obj.listen(conexiones)
-
-archivolog = os.path.join(path_logs, datetime.now())
-file = open(archivolog, "w")
-
-#No se me ocurrió otra manera de identificar el cliente
-cliente=obj.__getattribute__('name')
-
-print("Conectado al servidor")
+import threading
 
 
-while True:
-    mens = input("Mensaje de confirmacion. Listo para recibir un archivo")
-    obj.send(mens.encode('ascii'))
-    recibido = obj.recv(4096)
-#    log="nombre del archivo: "
-#    log=log+" Tamaño: "+os.stat(recibido).st_size
-#    log=log+"\n Cliente: "+cliente
-    nombre_archivo = cliente+"-Prueba"+conexiones
-    archivoPorEscribir = os.path.join(path_archivos, nombre_archivo)
-    file1 = open(archivoPorEscribir, "w")
-    file1.write(recibido)
-    file1.close()
-#    log=log+"\n Entrega exitosa: "
+def conexion():
+    host = 'localhost'
+    port = 8050
+    path_archivos = "./ArchivosRecibidos"
+    path_logs = "./Logs"
 
+    BLOCK_SIZE = 65536
+    conexiones = 25
+    obj = socket.socket()
 
-    file_hash = hashlib.sha256()
-    with open(archivoPorEscribir, 'rb') as f:
-        fb = f.read(BLOCK_SIZE)
-        while len(fb) > 0:
-            file_hash.update(fb)
+    obj.connect((host, port))
+    obj.listen(conexiones)
+
+    year=datetime.now().year
+    mes = datetime.now().month
+    dia = datetime.now().day
+    hora= datetime.now().hour
+    min = datetime.now().minute
+    seg = datetime.now().second
+
+    fecha= "/"+str(year)+"-"+str(mes)+"-"+str(dia)+"-"+str(hora)+"-"+str(min)+"-"+str(seg)+"log"
+    print(fecha)
+    archivolog = path_logs+fecha+".txt"
+    file = open(archivolog, "w")
+
+    cliente= threading.currentThread().getName()
+    print("Cliente "+cliente)
+    print("Conectado al servidor")
+
+    while (True):
+        mens = input("Mensaje de confirmacion. Listo para recibir un archivo")
+        obj.send(mens.encode('ascii'))
+        recibido = obj.recv(4096)
+        log = "nombre del archivo: "
+        log=log+" Tamaño: "+os.stat(recibido).st_size
+        log = log+"\n Cliente: "+cliente
+        nombre_archivo = cliente + "-Prueba" + str(conexiones)
+        print(nombre_archivo)
+        archivoPorEscribir = os.path.join(path_archivos, nombre_archivo)
+        file1 = open(archivoPorEscribir, "w")
+        file1.write(recibido)
+        file1.close()
+        log = log+"\n Entrega exitosa: "
+
+        file_hash = hashlib.sha256()
+        with open(archivoPorEscribir, 'rb') as f:
             fb = f.read(BLOCK_SIZE)
-    resultadoHash = file_hash.hexdigest()
-#log=log+"\n Tiempo de transferencia: "
-#log=log+"\n Número de paquetes recibidos: "++" número de bytes recibidos: "
-obj.close()
-print("Conexión cerrada")
+            while len(fb) > 0:
+                file_hash.update(fb)
+                fb = f.read(BLOCK_SIZE)
+        resultadoHash = file_hash.hexdigest()
+        log=log+"\n Tiempo de transferencia: "
+        log=log+"\n Número de paquetes recibidos: "+" número de bytes recibidos: "
+        file.write(log)
+        file.close()
+    obj.close()
+    print("Conexión cerrada")
 
 
-if __name__ == '__main__':
-    print_hi('Cliente')
+NUM_HILOS = 25
 
+for num_hilo in range(NUM_HILOS):
+    hilo = threading.Thread(target=conexion,name='Cliente'+str(num_hilo))
+    hilo.start()
 
